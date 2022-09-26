c33cc3cec62d01548506f24dec89d501d689cc7e
kaixinxiaole <2279596884@qq.com>
2022-09-26 16:44:25 +0800

    First Commit

---
+++ demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<!--  写见面-->
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 声明数据，方法
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/*样式表*/
+
+</style>
