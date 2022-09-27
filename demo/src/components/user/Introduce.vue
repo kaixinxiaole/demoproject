7f59a584457d3be291a354468b0a92c76cc04ee4
kaixinxiaole <2279596884@qq.com>
2022-09-27 08:40:26 +0800

    demo

---
+++ demo/src/components/user/Introduce.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <el-descriptions title="个人信息" direction="vertical" :column="4" border>
+    <el-descriptions-item label="用户名">{{this.name}}</el-descriptions-item>
+    <el-descriptions-item label="姓名">游**</el-descriptions-item>
+    <el-descriptions-item label="年龄">21</el-descriptions-item>
+    <el-descriptions-item label="手机号">178****9012</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="居住地" :span="2">南昌市</el-descriptions-item>
+    <el-descriptions-item label="联系地址">江西省南昌市东湖区江西师范大学</el-descriptions-item>
+    <el-descriptions-item label="年级">大三</el-descriptions-item>
+  </el-descriptions>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Introduce",
+  data (){
+    return{
+      name:this.$route.query.id,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo/src/components/user/Login.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="building">
+    <div id="poster">
+      <el-form class="login-container" label-position="left" label-width="0px">
+        <h3 class="login_title">
+          欢迎登录
+          <el-button @click="toRegister()">前往注册</el-button>
+        </h3>
+        <el-form-item label="">
+          <el-input type="text" v-model="loginname" autocomplete="off" placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" v-model="password" autocomplete="off" placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item style="width: 100%;">
+          <el-button type="primary" style="width: 100%;background: aquamarine;border: none" @click="Login">立即登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data(){
+      return{
+          password:"",
+          loginname:""
+      }
+    },
+    methods:{
+      toRegister(){
+        this.$router.push({path:'/Register'});
+        this.$message({
+          message:'请注册',
+          type:'success'
+        })
+      },
+      Login(){
+        this.$router.push({path:'/Introduce',query:{id:this.loginname}});
+        //获取用户输入数据
+        // this.$http.get('/Introduce',{params:{"userName":this.loginname,"password":this.password}}).then(function(res){
+        //   alert("登录状态:"+res.body);
+        //   document.write(res.body);
+        // },function (){
+        //   console.log("请求失败处理");
+        // });
+        this.$message({
+          message:'登录成功',
+          type:'success'
+        });
+        this.$http.get()
+      }
+
+
+    }
+}
+</script>
+
+<style scoped>
+#poster{
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: fixed;
+  margin: 0px;
+  padding: 0px;
+}
+
+.login-container{
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 90px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #cac6c6;
+  box-shadow: 0 0 25px #cac6c6;
+}
+.login_title{
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: dimgrey;
+}
+#building{
+  background-image: linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%);
+  width:100%;
+  height:100%;
+  position:fixed;
+  background-size:100% 100%;
+}
+
+</style>
